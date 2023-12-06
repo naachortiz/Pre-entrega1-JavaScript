7506434a78ae9818b8446848bfc2ad54aa5636a0
Ignacio Ortiz <nachortiz191@gmail.com>
2023-12-06 12:46:37 -0300

    Agregado terminado código HTML, JavaScript y CSS

--- app.js
+++ app.js
@@ -6,78 +6,98 @@ document.addEventListener('DOMContentLoaded', function () {
     const alertMessage = document.getElementById('alertMessage');
     const restartButton = document.getElementById('restartButton');
     const difficultySelect = document.getElementById('difficulty');
-  
-    // Pregunta el nombre del usuario
-    const userName = prompt('¡Hola! Ingresa tu nombre:');
-  
-    // Muestra el mensaje de bienvenida en la alerta 
-    if (userName) {
-      const welcomeMessage = `Bienvenido, ${userName}!`;
-      displayAlert(welcomeMessage);
-      alert(welcomeMessage);  
-    }
-  
-    let score = 0;
-    let maxAttempts = 3;
-    let minNumber = 1;
-    let maxNumber = 10;
-    let targetNumber;
-  
+
+    const game = {
+        score: 0,
+        maxAttempts: 3,
+        minNumber: 1,
+        maxNumber: 10,
+        targetNumber: 0,
+    };
+
     submitButton.addEventListener('click', function () {
         const userGuess = parseInt(inputData.value);
-  
+
         if (!isNaN(userGuess)) {
             checkGuess(userGuess);
         } else {
             displayAlert('Por favor, ingresa un número válido.');
         }
     });
-  
+
     restartButton.addEventListener('click', function () {
         startGame();
     });
-  
+
     function startGame() {
-        targetNumber = generateRandomNumber(minNumber, maxNumber);
-        maxAttempts = 3;
+        const selectedDifficulty = difficultySelect.value;
+        setDifficultyRange(selectedDifficulty);
+
+        game.targetNumber = generateRandomNumber(game.minNumber, game.maxNumber);
+        game.maxAttempts = 3;
         outputResult.innerHTML = '';
-        scoreDisplay.textContent = `Puntuación: ${score}`;
-        displayAlert(`Nivel ${maxNumber - minNumber + 1}: Adivina el número entre ${minNumber} y ${maxNumber}`);
+        scoreDisplay.textContent = `Puntuación: ${game.score}`;
+        displayAlert(`Nivel ${selectedDifficulty}: Adivina el número entre ${game.minNumber} y ${game.maxNumber}`);
     }
-  
-    function checkGuess(userGuess) {
-        maxAttempts--;
-  
-        if (userGuess === targetNumber) {
-            displayAlert(`¡Adivinaste! Has ganado ${maxAttempts + 1} puntos.`);
-            score += maxAttempts + 1;
-            startGame();
-        } else {
-            const message = userGuess < targetNumber ? 'El número es mayor.' : 'El número es menor.';
-            displayAlert(`${message} Intentos restantes: ${maxAttempts}`);
-            outputResult.innerHTML += `<p>Intento ${maxAttempts + 1}: ${userGuess}</p>`;
-  
-            if (maxAttempts === 0) {
-                displayAlert(`¡Agotaste tus intentos! El número correcto era ${targetNumber}.`);
-                startGame();
-            }
+
+    function setDifficultyRange(difficulty) {
+        switch (difficulty) {
+            case '1':
+                game.minNumber = 1;
+                game.maxNumber = 10;
+                break;
+            case '2':
+                game.minNumber = 1;
+                game.maxNumber = 50;
+                break;
+            case '3':
+                game.minNumber = 1;
+                game.maxNumber = 100;
+                break;
+            default:
+                game.minNumber = 1;
+                game.maxNumber = 10;
         }
-  
-        console.log(`Puntuación acumulativa: ${score}`);
     }
-  
+
+    function checkGuess(userGuess) {
+        game.maxAttempts--;
+
+        if (userGuess === game.targetNumber) {
+            game.score += game.maxAttempts + 1;
+            outputResult.innerHTML += `<p class="success">¡Felicidades! Has adivinado el número correcto: ${userGuess}</p>`;
+            alert(`¡Felicidades! Has adivinado el número correcto: ${userGuess}`);
+            startGame();
+            return;
+        }
+
+        const message = userGuess < game.targetNumber ? 'El número es mayor.' : 'El número es menor.';
+        displayAlert(`${message} Intentos restantes: ${game.maxAttempts}`);
+
+        outputResult.innerHTML += `<p>Intento ${3 - game.maxAttempts}: ${userGuess}</p>`;
+
+        if (game.maxAttempts === 0) {
+            displayAlert(`¡Agotaste tus intentos! El número correcto era ${game.targetNumber}.`);
+            alert(`¡Agotaste tus intentos! El número correcto era ${game.targetNumber}.`);
+            startGame();
+        }
+
+        console.log(`Puntuación acumulativa: ${game.score}`);
+    }
+
     function generateRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-  
+
     function displayAlert(message) {
         alertMessage.textContent = message;
         setTimeout(() => {
             alertMessage.textContent = '';
-        }, 5000); // Ocultar el mensaje después de 5 segundos
+        }, 5000);
     }
-  
+
     startGame();
-  });
+});
+
   
 
---
+++ readme.md
@@ -0,0 +1,37 @@
+# Guía de Uso - Simulador Interactivo
+
+¡ Bienvenido al Simulador Interactivo sobre juego de adivinanza !
+
+Este simulador ha sido diseñado para proporcionar una experiencia interactiva en línea. A continuación, encontrarás una guía rápida sobre cómo usar la aplicación.
+
+## Funcionalidades
+
+- **Selección de Dificultad:** Elige entre tres niveles de dificultad.
+- **Adivinanza:** Intenta adivinar el número correcto dentro del rango establecido.
+- **Historial de Intentos:** Muestra un registro de tus intentos anteriores.
+- **Puntuación:** Obtiene puntos por cada adivinanza correcta.
+- **Alertas y Mensajes:** Recibe mensajes sobre el progreso del juego.
+
+## Cómo Jugar
+
+1. Selecciona un nivel de dificultad.
+2. Ingresa tu suposición en el campo proporcionado.
+3. Haz clic en el botón "Adivinar".
+4. Recibe feedback sobre tu suposición.
+5. Repite hasta que adivines el número o agotes tus intentos.
+
+## Reiniciar el Juego
+
+Si deseas reiniciar el juego en cualquier momento:
+
+1. Haz clic en el botón "Reiniciar Juego".
+
+## Consideraciones
+
+- **Puntaje:** Tu puntuación se acumula con cada adivinanza correcta.
+- **Niveles de Dificultad:** Cada nivel ajusta el rango de números posibles.
+- **Limite de Intentos:** El número de intentos disponibles varía según el nivel.
+
+## Contacto
+
+Si tienes alguna pregunta o comentario, no dudes en ponerte en contacto conmigo. ¡Disfruta del juego!
